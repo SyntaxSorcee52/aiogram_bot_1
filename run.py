7ca534418ec548b3842fbef21eaa8803ee9656c6
Krolik <sandw1chmastergg@gmail.com>
2026-08-06 19:33:11 +0300

    feat: start

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 import asyncio
 import aiogram
 from aiogram import Bot , Dispatcher
-import sys
+from aiogram.filters import Command
+from aiogram.types import Message
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+@dp.message(Command(commands='start'))
+async def cmd_start(message: Message):
+    await message.answer('hello ')
+
+
 
 async def main():
     await dp.start_polling(bot)
